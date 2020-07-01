d0bacd5847fba219a107bb8a1e6649cd19728ca9
unknown <katharina.r@hotmail.fr>
2020-07-01 17:59:13 +0200

    up

--- views/dilex.php
+++ views/dilex.php
@@ -17,6 +17,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\2.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Serviettes hygiéniques taille M</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -27,6 +28,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\3.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Serviettes hygiéniques taille L</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -37,6 +39,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\4.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Protège-slip ultra fin</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -47,6 +50,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\5.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>100 Mouchoirs, double épaisseur</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -57,6 +61,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\6.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>100 Mouchoirs, double épaisseur</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -67,6 +72,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\7.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Coton-tige x200</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -77,6 +83,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\8.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Lingettes hydratantes pour le visage et les mains</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -87,6 +94,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\9.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Lingettes démaquillantes pour tous types de peau</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -97,6 +105,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\10.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p> Lingettes intimes à l'huile de noisette</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -107,6 +116,7 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\11.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Lingettes hygiéniques pour homme</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
@@ -117,14 +127,13 @@
                     <figcaption class="overlay">
                         <a href="#"><img src="img\DILEX\12.jpg" width="350" height="300" alt="dilex"></a>
                         <a class="link" href="#"><p>Lingettes démaquillantes x70</p></a>
+                        <button id="myBtn">Voir le produit</button>
                         <p>Prix</p>
                     </figcaption>
                 </figure>               
             </div>
 
         </div>
-
-<!-- Trigger/Open The Modal -->
 
 
 <!-- The Modal -->
@@ -138,7 +147,6 @@
     </div>
     <div class="modal-body">
       <p>Description text</p>
-      <p>Some other text...</p>
     </div>
     <div class="modal-footer">
       <h3>Prix : xx €</h3>
