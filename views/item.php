f7f1a39b1264cb8659060e5fd4888a91ab5d2538
unknown <katharina.r@hotmail.fr>
2020-07-08 12:34:07 +0200

    update

--- views/item.php
+++ views/item.php
@@ -1,24 +1,52 @@
+<?php
+
+$item = [
+    "id" => 1,
+    "nom" => "Lingettes hydratantes",
+    "image1" => "img\DILEX\1.jpg",
+    "image2" => "img\DILEX\1_2.jpg",
+    "image3" => "img\DILEX\1_3.jpg",
+    "description" => "Les lingettes nettoyantes et désinfectantes avec huile d'argan, huile d'amande, vitamine E, sans paraben.</p>
+    <p>Appropriés lorsqu'il n'y a pas de conditions de toilette, idéales pour les soins des malades et des personnes âgées.</p>
+    <p>Produit convient pour une utilisation quotidienne, ne dessèche pas la peau.",
+    "prix" => 15
+];
+
+
+$donnees = [
+    
+];
+
+
+?>
+
+
 <h1>DILEX</h1>
-<div class="name"><h2>Lingettes hydratantes<h2></div>
+<?php foreach($donnees as $donnee): ?>
+<div class="name"><h2><?= $donnee["nom"]?><h2></div>
 <div class="product">
     <div class="item1">
-        <img src="img\DILEX\1.jpg" width="350" height="300" alt="dilex">
+        <img src="img\<?= $donnee["image1"]?>" width="175" height="150" alt="dilex">
     </div>
 
-    <div class="item">
-        <img src="img\DILEX\1_2.jpg" width="175" height="150" alt="dilex">
+    <div class="item1">
+        <img src="img\<?= $donnee["image2"]?>" width="175" height="150" alt="dilex">
     </div>
 
-    <div class="item">
-        <img src="img\DILEX\1_3.jpg" width="175" height="150" alt="dilex">
+    <div class="item1">
+        <img src="img\<?= $donnee["image3"]?>" width="175" height="150" alt="dilex">
     </div>
+
+<div class="description"><p><?= $donnee["description"]?></p></div>
+
+<div class="prix">
+    <p><?= $donnee["prix"]?></p>
 </div>
 
-<div class="description"><p>Les lingettes nettoyantes et désinfectantes avec huile d'argan, huile d'amande, vitamine E, sans paraben.</p>
-<p>Appropriés lorsqu'il n'y a pas de conditions de toilette, idéales pour les soins des malades et des personnes âgées.</p>
-<p>Produit convient pour une utilisation quotidienne, ne dessèche pas la peau.</p></div>
+<?php endforeach ?>
 
-<div class="prix"><p>Prix : ... €</p></div>
+
+
 
 
 
