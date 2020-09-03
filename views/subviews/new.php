b4f6cf2913d743e1c90c48a200c66c8e3a347b14
Nathaelle <scriptales.clients@gmail.com>
2020-09-03 15:49:36 +0200

    modif best:new sur accueil -> simplification

--- views/subviews/new.php
+++ views/subviews/new.php
@@ -1,32 +1,26 @@
 <?php
-$items = $view['datas']['best_items'];
-$images = $view['datas']['best_images'];
+$n_items = $view['datas']['new_items'];
 ?>
 
 <div class="selection">
     <div class="container">
 
-        <?php foreach($items as $item): ?>
+        <?php foreach($n_items as $item): ?>
 
             <div class="produit">
                 <div class="photo"> 
                     <div class="overlay">
 
                         <a href="index?route=item">
-                            <?php foreach($images as $image) : ?>
-                                <?php if($item->getIdItem() == $image->getIdItem()) :?>
-                                    <p>Note : <?= $item->getNote()?></p>
-                                <img src="img/<?=$image->getName(); ?>">
-                                <?php break; ?>
-                                <?php endif; ?>   
-                            <?php endforeach?>
+                            <?php if(isset($item->images[0])) : ?>
+                                <img src="img/<?=$item->images[0]->getName(); ?>">
+                            <?php endif?>
                         </a>
 
                         <a class="link" href="index?route=item&id=<?= $item->getIdItem()?>"><p><?= $item->getName()?></p></a>
 
                         <button class="myBtn" id="myBtn-<?= $item->getIdItem()?>">Voir le produit</button>
 
-                        
                         <p>Prix : <?= $item->getPrice()?> €</p>
 
                     </div>
